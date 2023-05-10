1418e49c727e6ccaa2bdc4f664a72e470dd4af87
Gaurav singh <xogauravsinghxo@gmail.com>
2023-05-10 15:14:54 +0530

    new

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key=env.AWS_ACCESS_KEY_ID
-  secret_key=env.AWS_SECRET_ACCESS_KEY
+  access_key=AWS_ACCESS_KEY_ID
+  secret_key=AWS_SECRET_ACCESS_KEY
   region     = "us-east-1" 
 }
 
