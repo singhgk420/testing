f6c9ee441e7e2722b84662265beb7c5d4d565984
Gaurav singh <xogauravsinghxo@gmail.com>
2023-05-10 14:33:33 +0530

    new

--- main.tf
+++ main.tf
@@ -1,9 +1,6 @@
 provider "aws" {
   
-  region     = "us-east-1"
-  access_key =$AWS_ACCESS_KEY_ID
-  secret_key =$AWS_SECRET_ACCESS_KEY
-  
+  region     = "us-east-1" 
 }
 
 resource "aws_instance" "example" {
